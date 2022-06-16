088456af6af8fec04658aff36cf239af18056120
Shubh118 <npandey5ideal@gmail.com>
2022-06-16 19:11:56 +0530

    shubham

---
+++ skin.js
@@ -0,0 +1,241 @@
+let skinData = [
+    {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/NicoLips-01_77788ac8-4061-459a-bfb5-f5b5068c1dfa_700x.jpg?v=1652340499",
+     name: "NICOLIP LIP BRIGHTENING SCRUB, 20GM",
+     description: "Lip Brightening, Nousrishing & Repairing",
+     price: "315",
+     stackedPrice: "₹ 450",
+     rating: "4.7 ★",
+     productID: "S-01",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/NicoLips-02_360x.jpg?v=1652340499"},
+     {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/EyeLift-01_4ac1ff22-cbb1-4b18-8b9e-11204b255113_700x.jpg?v=1652340587",
+     name: "EYELIFT UNDER EYE CREAM, 20GM",
+     description: "Repairs Dark Circles, Puffy Eyes & Wrinkles",
+     price: "228",
+     stackedPrice: "₹ 325",
+     rating: "4.7 ★",
+     productID: "S-02",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Eyelift-02_360x.jpg?v=1652340587"},
+     {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/C-GlowFaceWash-01_2_700x.jpg?v=1652340405",
+     name: "C-GLOW FACE WASH, 100ML",
+     description: "De-Tan & Brightening",
+     price: "174",
+     stackedPrice: "₹ 249",
+     rating: "4.7 ★",
+     productID: "S-03",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/C-GlowFaceWash-02_360x.jpg?v=1652340405"},
+     {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Kumkumadi-Face-Oil-01_700x.jpg?v=1652341175",
+     name: "AYURVEDIC KUMKUMADI FACE OIL, 30ml",
+     description: "Skin Brightening & Hydrating Oil For Youthful Skin",
+     price: "419",
+     stackedPrice: "₹ 599",
+     rating: "4.7 ★",
+     productID: "S-04",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/KumkumadiFaceOil-02_360x.jpg?v=1652341175"},
+     {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Anti-AcneFaceGel-01_0b665e08-c71f-409a-82ef-10b68c67e453_700x.jpg?v=1651699935",
+     name: "ANTI ACNE, PIMPLE SCAR SPOTS REMOVAL GEL FOR MEN & WOMEN, 50gm",
+     description: "Pimple Scar Removal & Skin Clarifying",
+     price: "279",
+     stackedPrice: "₹ 399",
+     rating: "4.7 ★",
+     productID: "S-05",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Anti-AcneFaceGel-02_1_360x.jpg?v=1652075347"},
+     {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/KumkumadiFaceWash-01_1_700x.jpg?v=1652341314",
+     name: "KUMKUMADI FACE WASH - 100ml",
+     description: "Pore Cleansing, Radiating & Skin Rejuvenating",
+     price: "279",
+     stackedPrice: "₹ 399",
+     rating: "4.7 ★",
+     productID: "S-06",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/KumkumadiFaceWash-02_360x.jpg?v=1652341314"},
+     {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/NicoBalm-01_6c683236-3694-4219-8fee-e969bbf9e94d_700x.jpg?v=1640436778",
+     name: "THE LIP BALM - 5gm",
+     description: "Hydrating & Nourishing",
+     price: "158",
+     stackedPrice: "₹ 225",
+     rating: "4.7 ★",
+     productID: "S-07",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/NicoBalm-02_93beb060-c566-487f-91cf-254d5f81c1e6_360x.jpg?v=1641286093"},
+     {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Anti-Acne-Face-wash_-Anti-Acne-Face-Gel-Cream-Combo_700x.jpg?v=1647864525",
+     name: "ANTI ACNE, PIMPLE SCARS SPOTS REMOVAL COMBO",
+     description: "Anti Acne, Pimple Scar Spots Removal Combo",
+     price: "489",
+     stackedPrice: "₹ 698",
+     rating: "4.7 ★",
+     productID: "S-08",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Acne-Be-Gone-Combo-02_360x.jpg?v=1651698205"},
+     {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/De-TanFacePack-01_700x.jpg?v=1652168815",
+     name: "ANTI ACNE, PIMPLE SCARS SPOTS REMOVAL COMBO",
+     description: "Anti Acne, Pimple Scar Spots Removal Combo",
+     price: "279",
+     stackedPrice: "₹ 399",
+     rating: "4.7 ★",
+     productID: "S-09",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/De-TanFacePack-02_360x.jpg?v=1652168815"},
+     {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/AntiAcneFaceWash-01_700x.jpg?v=1651698225",
+     name: "DE TAN FACE PACK FOR GLOWING SKIN,100gm",
+     description: "Detan & Skin Brightening",
+     price: "209",
+     stackedPrice: "₹ 299",
+     rating: "4.7 ★",
+     productID: "S-10",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Anti-AcneFaceWash-02_f9aa022d-5d4e-4703-b889-7c62aa59b206_360x.jpg?v=1652182647"},
+     {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/C-GlowFaceSerum-01_1_700x.jpg?v=1651698248",
+     name: "C-GLOW FACE SERUM ,30ml",
+     description: "Brightening & Anti Pigmentation",
+     price: "263",
+     stackedPrice: "₹ 599",
+     rating: "4.7 ★",
+     productID: "S-11",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/C-GlowFaceSerum-06_9d045130-dcb9-4c59-aa41-12ef9cafcdd1_360x.jpg?v=1652182969"},
+     {imageUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/C-Glow-Face-Wash_-Exfoliate-Scrub-Combo_700x.jpg?v=1647864479",
+     name: "NATURAL ANTI POLLUTION COMBO",
+     description: "C-Glow Face Wash & Exfoliate Face & Body Scrub",
+     price: "367",
+     stackedPrice: "₹ 599",
+     rating: "4.7 ★",
+     productID: "S-12",
+     hoverUrl: "https://cdn.shopify.com/s/files/1/0054/6665/2718/products/Brighter-Days-Combo-02_360x.jpg?v=1651698233"},
+]
+
+let cart = JSON.parse(localStorage.getItem("cartData")) || [];
+
+skinData.forEach(display);
+
+function display(elem){
+    
+
+    let div = document.createElement("div");
+    
+    
+    let img = document.createElement("img");
+    img.setAttribute("src",elem.imageUrl);
+
+    let name = document.createElement("h5");
+    name.innerText = elem.name
+
+    let description = document.createElement("p");
+    description.innerText = elem.description
+
+    let prdiv = document.createElement("div");
+
+    let price = document.createElement("h4");
+
+    let rupee = document.createElement("h4");
+    rupee.innerText = "₹"
+    
+    price.innerText = elem.price;
+
+    let stackedprice = document.createElement("p");
+    stackedprice.innerText = elem.stackedPrice;
+    
+
+    let rating= document.createElement("h3");
+    rating.innerText = elem.rating;
+
+
+    let cartButton = document.createElement("button");
+    cartButton.innerText = "ADD TO CART";
+
+    document.querySelector("#container").append(div)
+    div.append(img,name,description,prdiv,cartButton)
+    prdiv.append(rupee,price,stackedprice,rating)
+
+    cartButton.addEventListener("click",function(){
+        if(check()){
+            alert("ALREADY ADDED TO CART")
+        }else{
+            cart.push(elem)
+        localStorage.setItem("cartData",JSON.stringify(cart))
+        window.location.href="cart.html"
+        }
+        
+    })
+
+    function check(){
+        for(let i=0; i<cart.length; i++){
+            if(cart[i].productID == elem.productID){
+                return true;
+            }
+        }
+        return false;
+    }
+    div.addEventListener("mouseover",mouseOn)
+    function mouseOn(){
+        div.style.cursor = "pointer"
+        img.setAttribute("src",elem.hoverUrl)
+    }
+
+    div.addEventListener("mouseout",mouseoff)
+    function mouseoff(){
+        img.setAttribute("src",elem.imageUrl)
+    }
+
+    img.addEventListener("click",redirect)
+    name.addEventListener("click",redirect)
+
+    function redirect(){
+        localStorage.setItem("desciption",JSON.stringify(elem))
+        window.location.href = "skin-description/s-12.html"
+    }
+
+
+    // /////////////////////////////////////////
+
+    document.querySelector("#featured").addEventListener("change", function(){
+        filterit(document.querySelector("#featured").value,skinData)
+    })
+    /* filtred run program */
+    function filterit(getvalue,skinData){
+        if(getvalue=="Alpha"){
+            filterbyAZ(skinData)
+        }else if(getvalue=="rev Alpha"){
+            filterbyZA(skinData)
+        }else if(getvalue=="LH"){
+            filterbyLH(skinData)
+        }else if(getvalue=="HL"){
+            filterbyHL(skinData)
+        }
+    }
+    /* Sorting Functions */
+    /* Alphabatic */
+    function filterbyAZ(skinData){
+        
+        let filter = skinData.sort(function(a,b){
+            if(a.name>b.name) return 1
+            if(a.name<b.name) return -1
+            return 0
+        })
+        document.querySelector("#container").innerHTML = ""
+        filter.forEach(display)
+    }
+    function filterbyZA(skinData){
+        let filter = skinData.sort(function(a,b){
+            if(a.name>b.name) return -1
+            if(a.name<b.name) return 1
+            return 0
+        })
+        document.querySelector("#container").innerHTML = ""
+        filter.forEach(display)
+    }
+    /* Price based */
+    function filterbyLH(skinData){
+        let filter = skinData.sort(function(a,b){
+            return a.price - b.price
+        })
+        document.querySelector("#container").innerHTML = ""
+        filter.forEach(display)
+    }
+    function filterbyHL(skinData){
+        let filter = skinData.sort(function(a,b){
+            return b.price - a.price
+        })
+        document.querySelector("#container").innerHTML = ""
+        filter.forEach(display)
+    }
+}
+
+
+
+
+
+
